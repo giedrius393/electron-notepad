9ab4639605c5c2342dd432b170593eb993cacaa6
Giedrius Jocius <giedrius393@gmail.com>
2023-08-16 23:37:48 +0300

    Add event handling for InputBox

--- src/components/InputBox/InputBox.tsx
+++ src/components/InputBox/InputBox.tsx
@@ -1,7 +1,23 @@
+import { SyntheticEvent } from 'react';
 import styles from './InputBox.module.css';
 
-const InputBox = () => {
-  return <div className={styles.body} contentEditable />;
+interface InputBoxProps {
+  onTextChange?: (text: string) => void;
+}
+
+const InputBox = ({ onTextChange }: InputBoxProps) => {
+  const handleInput = (e: SyntheticEvent) => {
+    const target = e.target as HTMLInputElement;
+    onTextChange(target.innerText);
+  }
+
+  return (
+    <div
+      className={styles.body}
+      contentEditable
+      onInput={handleInput}
+    />
+  );
 };
 
 export default InputBox;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,11 @@
 import InputBox from '../../components/InputBox/InputBox';
+import { useState } from "react";
 
 const Main = () => {
-  return <InputBox />;
+  const [text, setText] = useState('');
+  console.log(text);
+
+  return <InputBox onTextChange={setText}/>;
 };
 
 export default Main;
